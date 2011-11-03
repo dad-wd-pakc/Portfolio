1bcac76428b1d65f7e85c453c9738ea1ea628e79
dad-wd-pakc <pak-loon.cheng@digitalanddirect.com>
2011-11-03 17:59:36 +0000

    Scriptalicious

--- js/global.js
+++ js/global.js
@@ -14,11 +14,26 @@ var initialiseBlogPosts = function() {
 };
 
 var initialiseProjects = function() {
-	var i = 0;
+	var i, theReviews, j, k;
 	$.getJSON("js/projects.js", function(json) {
 		$.each(json.projects, function (i) {
-			console.log("JSON Data: " + json.projects[i].title);
+			$("body").append("<p>" + json.projects[i].title + "</p>");
 			
+			$.each(json.projects[i].reviews, function (i) {
+				$("body").append("<p>" + json.projects[i].reviews.text + "</p>");
+				theReviews = json.projects[i].reviews;
+				console.log(theReviews[i]);
+				
+				for (j = 0; j < theReviews[j].text[j].length; j++) {
+					console.log(theReviews[j].text[]);
+				}
+				
+				//for (j = 0; j < theReviews[i].length; j++) {
+					//for (k = 0; k < theReviews[i].text.length; k++) {
+						//$("body").append("<p>" + theReviews[i].text + "</p>");
+					//}
+				//}
+			});
 		});
 	})	
 	.success(function() { console.log("Success"); })
--- js/projects.js
+++ js/projects.js
@@ -1,4 +1,9 @@
 {"projects":[
-{"title": "Friday the 13th", "year": 1980, "reviews": [{"reviewer": "Pam", "stars": 3, "text": "Pretty good, but could have used more Jason"}, {"reviewer": "Alice", "stars": 4, "text": "The end was good, but a little unsettling"}]},
-{"title": "Friday the 15th", "year": 1986, "reviews": [{"reviewer": "Pim", "stars": 2, "text": "Not too good, but could have used less Jason"}, {"reviewer": "Alyce", "stars": 1, "text": "The end was bad, not at all unsettling"}]}
+{"title": "Friday the 13th", "year": 1980, "reviews": [{"reviewer": "Pam", "stars": 3, "text": "Mongfest"}, {"reviewer": "Alice", "stars": 4, "text": "Spackerfest"}]},
+{"title": "Friday the 14th", "year": 1981, "reviews": [{"reviewer": "Dirk", "stars": 3, "text": "Twatfest"}, {"reviewer": "Steve", "stars": 4, "text": "Rumfest"}]},
+{"title": "Friday the 15th", "year": 1982, "reviews": [{"reviewer": "Dennis", "stars": 3, "text": "Screamfest"}, {"reviewer": "Kate", "stars": 4, "text": "Dumbfest"}]},
+{"title": "Friday the 16th", "year": 1983, "reviews": [{"reviewer": "Jennifer", "stars": 3, "text": "Nunfest"}, {"reviewer": "Stuart", "stars": 4, "text": "Scumfest"}]},
+{"title": "Friday the 17th", "year": 1984, "reviews": [{"reviewer": "Dominic", "stars": 3, "text": "Chavfest"}, {"reviewer": "Brenda", "stars": 4, "text": "Bitchfest"}]},
+{"title": "Friday the 18th", "year": 1985, "reviews": [{"reviewer": "Linda", "stars": 3, "text": "Dancefest"}, {"reviewer": "Bert", "stars": 4, "text": "Gunfest"}]},
+{"title": "Friday the 19th", "year": 1986, "reviews": [{"reviewer": "Pim", "stars": 2, "text": "Bumfest"}, {"reviewer": "Alyce", "stars": 1, "text": "Crumbfest"}]}
 ]}
