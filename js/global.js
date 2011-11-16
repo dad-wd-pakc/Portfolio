560d9ef11631e981b7be2c76be812fb127eddb74
dad-wd-pakc <pak-loon.cheng@digitalanddirect.com>
2011-11-16 14:39:47 +0000

    Responsive images

--- js/global.js
+++ js/global.js
@@ -1,18 +1,19 @@
+window.onload = function() {
+	//initialiseBlogPosts();
+	//initialiseProjects();
+}
+
 /*
 $(document).ready(function() {
 	initialiseBlogPosts();
 });
 */
 
-window.onload = function() {
-	//initialiseBlogPosts();
-	//initialiseProjects();
-}
 
 var initialiseBlogPosts = function() {
 
 };
-
+/*
 var initialiseProjects = function() {
 	var i, theReviews, j, k;
 	$.getJSON("js/projects.js", function(json) {
@@ -27,16 +28,17 @@ var initialiseProjects = function() {
 				for (j = 0; j < theReviews[j].text[j].length; j++) {
 					console.log(theReviews[j].text[]);
 				}
-				
+*/				
 				//for (j = 0; j < theReviews[i].length; j++) {
 					//for (k = 0; k < theReviews[i].text.length; k++) {
 						//$("body").append("<p>" + theReviews[i].text + "</p>");
 					//}
 				//}
-			});
+/*			});
 		});
 	})	
 	.success(function() { console.log("Success"); })
 	.error(function() { console.log("Error"); })
 	.complete(function() { console.log("Complete"); });
 };
+*/
