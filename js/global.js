93c9c9e3c027203fdd16e6218af3fb27f27fba5e
dad-wd-pakc <pak-loon.cheng@digitalanddirect.com>
2011-12-20 18:21:08 +0000

    Playing around with some JSON

--- js/global.js
+++ js/global.js
@@ -1,44 +1,59 @@
-window.onload = function() {
-	//initialiseBlogPosts();
-	//initialiseProjects();
-}
-
 /*
-$(document).ready(function() {
-	initialiseBlogPosts();
-});
+window.onload = function() {
+//initialiseBlogPosts();
+//initialiseProjects();
+}
 */
 
+$(document).ready(function() {
+	initialiseTheScripts();
+});
 
-var initialiseBlogPosts = function() {
+var initialiseTheScripts = function () {
+    var currentPage = $("body").attr("id");
+    switch (currentPage) {
+        case "home":
+            initialiseGeneral();
+            initialiseHome();
+            break;
+        case "about":
+            initialiseGeneral();
+            initialiseAbout();
+            break;
+        case "contact":
+            initialiseGeneral();
+            initialiseContact();
+            break;
+        default:
+            initialiseGeneral();
+    }
 
 };
-/*
+
+var intialiseGeneral = function () {
+    
+};
+
+var initialiseHome = function () {
+    initialiseProjects();
+};
+
+var initialiseAbout = function () {
+
+};
+
+var initialiseContact = function () {
+
+};
+
 var initialiseProjects = function() {
 	var i, theReviews, j, k;
 	$.getJSON("js/projects.js", function(json) {
 		$.each(json.projects, function (i) {
 			$("body").append("<p>" + json.projects[i].title + "</p>");
-			
-			$.each(json.projects[i].reviews, function (i) {
-				$("body").append("<p>" + json.projects[i].reviews.text + "</p>");
-				theReviews = json.projects[i].reviews;
-				console.log(theReviews[i]);
-				
-				for (j = 0; j < theReviews[j].text[j].length; j++) {
-					console.log(theReviews[j].text[]);
-				}
-*/				
-				//for (j = 0; j < theReviews[i].length; j++) {
-					//for (k = 0; k < theReviews[i].text.length; k++) {
-						//$("body").append("<p>" + theReviews[i].text + "</p>");
-					//}
-				//}
-/*			});
 		});
 	})	
 	.success(function() { console.log("Success"); })
 	.error(function() { console.log("Error"); })
 	.complete(function() { console.log("Complete"); });
 };
-*/
--- js/projects.js
+++ js/projects.js
@@ -1,9 +1,52 @@
-{"projects":[
-{"title": "Friday the 13th", "year": 1980, "reviews": [{"reviewer": "Pam", "stars": 3, "text": "Mongfest"}, {"reviewer": "Alice", "stars": 4, "text": "Spackerfest"}]},
-{"title": "Friday the 14th", "year": 1981, "reviews": [{"reviewer": "Dirk", "stars": 3, "text": "Twatfest"}, {"reviewer": "Steve", "stars": 4, "text": "Rumfest"}]},
-{"title": "Friday the 15th", "year": 1982, "reviews": [{"reviewer": "Dennis", "stars": 3, "text": "Screamfest"}, {"reviewer": "Kate", "stars": 4, "text": "Dumbfest"}]},
-{"title": "Friday the 16th", "year": 1983, "reviews": [{"reviewer": "Jennifer", "stars": 3, "text": "Nunfest"}, {"reviewer": "Stuart", "stars": 4, "text": "Scumfest"}]},
-{"title": "Friday the 17th", "year": 1984, "reviews": [{"reviewer": "Dominic", "stars": 3, "text": "Chavfest"}, {"reviewer": "Brenda", "stars": 4, "text": "Bitchfest"}]},
-{"title": "Friday the 18th", "year": 1985, "reviews": [{"reviewer": "Linda", "stars": 3, "text": "Dancefest"}, {"reviewer": "Bert", "stars": 4, "text": "Gunfest"}]},
-{"title": "Friday the 19th", "year": 1986, "reviews": [{"reviewer": "Pim", "stars": 2, "text": "Bumfest"}, {"reviewer": "Alyce", "stars": 1, "text": "Crumbfest"}]}
-]}
+[
+    {
+        "title": "Friday the 13th",
+        "year": 1980,
+        "reviews": {[
+            {"reviewer": "Pam", "stars": 3, "text": "Pie gummi bears chocolate cupcake chocolate bar tiramisu. Jujubes pastry ice cream cupcake tiramisu candy canes cupcake caramels chocolate cake. Biscuit croissant topping. Chocolate bar wafer carrot cake gummies."},
+            {"reviewer": "Alice", "stars": 4, "text": "Candy canes candy canes applicake lollipop bonbon donut chocolate bonbon chocolate. Carrot cake candy canes chupa chups jelly-o brownie apple pie jelly cupcake. Sweet fruitcake jelly-o pie sugar plum carrot cake icing biscuit."}
+        ]},
+    {
+        "title": "Friday the 14th",
+        "year": 1981,
+        "reviews": {[
+        {"reviewer": "Dirk", "stars": 3, "text": "Jujubes cupcake chocolate powder. Gingerbread cotton candy pie wafer bear claw jelly-o fruitcake. Bonbon candy jujubes oat cake tiramisu applicake."},
+        {"reviewer": "Steve", "stars": 4, "text": "Cookie tiramisu dessert jelly beans. Croissant soufflé pie marshmallow. Pie tootsie roll jujubes. Lollipop gingerbread pudding."}
+    ]},
+    {
+        "title": "Friday the 15th",
+        "year": 1982,
+        "reviews": {[
+        {"reviewer": "Dennis", "stars": 3, "text": "Tiramisu sugar plum chocolate bar wafer oat cake cotton candy lollipop croissant donut. Tiramisu donut chocolate muffin marzipan halvah candy halvah. Faworki cotton candy candy faworki candy canes chocolate cake jelly-o fruitcake jelly-o."},
+        {"reviewer": "Kate", "stars": 4, "text": "Pie sweet roll marshmallow bonbon brownie candy fruitcake. Bonbon chupa chups dessert. Sesame snaps candy canes chocolate bar topping."}
+    ]},
+    {
+        "title":
+        "Friday the 16th",
+        "year": 1983,
+        "reviews": {[
+            {"reviewer": "Jennifer", "stars": 3, "text": "Applicake fruitcake tart chocolate bar candy. Applicake croissant liquorice. Jelly-o gummi bears tootsie roll wafer ice cream."},
+            {"reviewer": "Stuart", "stars": 4, "text": "Pastry caramels jujubes applicake jujubes macaroon cake jelly. Lemon drops sweet roll powder sweet gingerbread lemon drops. Bear claw caramels marshmallow carrot cake."}
+        ]},
+    {
+        "title": "Friday the 17th",
+        "year": 1984,
+        "reviews": {[
+            {"reviewer": "Dominic", "stars": 3, "text": "Tootsie roll topping halvah cake tart oat cake carrot cake marshmallow marshmallow. Liquorice macaroon powder pudding cotton candy dessert wafer pie. Tootsie roll tart tiramisu bonbon lollipop sesame snaps pudding marshmallow. Icing chocolate oat cake sweet roll dragée jujubes pie marshmallow."},
+            {"reviewer": "Brenda", "stars": 4, "text": "Marshmallow chocolate bar marshmallow. Faworki danish icing jujubes lollipop pudding sugar plum. Bonbon muffin lemon drops muffin jelly-o faworki applicake carrot cake danish. Pie bonbon jelly gummi bears sugar plum muffin sweet roll ice cream chocolate bar."}
+    ]},
+    {
+        "title": "Friday the 18th",
+        "year": 1985,
+        "reviews": {[
+            {"reviewer": "Linda", "stars": 3, "text": "Faworki tiramisu jelly muffin ice cream cake cake. Bear claw toffee lemon drops brownie sesame snaps lemon drops. Fruitcake sweet sugar plum pie powder."},
+            {"reviewer": "Bert", "stars": 4, "text": "Lollipop liquorice icing bonbon tart tiramisu sweet roll powder. Macaroon toffee gummi bears danish powder dragée halvah lollipop. Sugar plum cake macaroon pastry pie macaroon pudding chocolate cake chocolate bar. Liquorice pudding lemon drops pastry lemon drops sweet roll wafer."}
+        ]},
+    {
+        "title": "Friday the 19th",
+        "year": 1986,
+        "reviews": {[
+            {"reviewer": "Pim", "stars": 2, "text": "Tiramisu bear claw apple pie topping cheesecake lollipop donut toffee chocolate cake. Bear claw icing bear claw lemon drops lemon drops cotton candy faworki fruitcake. Halvah halvah toffee marshmallow donut bonbon caramels chupa chups. Brownie gummies icing cotton candy donut."},
+            {"reviewer": "Alyce", "stars": 1, "text": "Tart danish apple pie. Chocolate cake bear claw sweet danish ice cream chocolate bar. Lollipop chocolate danish apple pie."}
+        ]}
+]
