f6da4617c5bd4df7f03873eff20a1e61aa4cc6a2
dad-wd-pakc <pak-loon.cheng@digitalanddirect.com>
2011-12-21 16:47:54 +0000

    JSON loading

---
+++ js/content.js
@@ -0,0 +1,83 @@
+{
+	"banners": [
+        { "banner": "banner01.jpg", "text": "Banksy next level irony echo park. Beard irony thundercats vice american apparel sustainable.", "link": "http://bkaprt.com/rwd/1"},
+		{ "banner": "banner02.jpg", "text": "Bicycle rights gentrify squid before they sold out.", "link": "http://bkaprt.com/rwd/2"},
+		{ "banner": "banner03.jpg", "text": "Cred fixie chambray sartorial, twee salvia tumblr thundercats.", "link": "http://bkaprt.com/rwd/3"},
+        { "banner": "banner04.jpg", "text": "Shoreditch readymade carles, dreamcatcher vegan +1 skateboard leggings sartorial stumptown photo booth aesthetic.", "link": "http://bkaprt.com/rwd/4"},
+		{ "banner": "banner05.jpg", "text": "Williamsburg before they sold out lomo tofu food truck hoodie.", "link": "http://bkaprt.com/rwd/5"},
+		{ "banner": "banner06.jpg", "text": "Thundercats tattooed art party vice, trust fund williamsburg yr.", "link": "http://bkaprt.com/rwd/6"}
+	],
+	"films": [
+		{
+			"title": "Prison Break",
+			"year": 1980,
+            "poster": "prisonbreak.jpg",
+			"reviews": [
+				{"reviewer": "Pam", "stars": 3, "text": "Pie gummi bears chocolate cupcake chocolate bar tiramisu. Jujubes pastry ice cream cupcake tiramisu candy canes cupcake caramels chocolate cake. Biscuit croissant topping. Chocolate bar wafer carrot cake gummies."},
+				{"reviewer": "Alice", "stars": 4, "text": "Candy canes candy canes applicake lollipop bonbon donut chocolate bonbon chocolate. Carrot cake candy canes chupa chups jelly-o brownie apple pie jelly cupcake. Sweet fruitcake jelly-o pie sugar plum carrot cake icing biscuit."}
+			],
+            "link": "http://bkaprt.com/rwd/7"
+		},
+		{
+			"title": "Sex and the City",
+			"year": 1981,
+            "poster": "sexandthecity.jpg",
+			"reviews": [
+				{"reviewer": "Dirk", "stars": 3, "text": "Jujubes cupcake chocolate powder. Gingerbread cotton candy pie wafer bear claw jelly-o fruitcake. Bonbon candy jujubes oat cake tiramisu applicake."},
+				{"reviewer": "Steve", "stars": 4, "text": "Cookie tiramisu dessert jelly beans. Croissant souffl&eacute; pie marshmallow. Pie tootsie roll jujubes. Lollipop gingerbread pudding."}
+			],
+            "link": "http://bkaprt.com/rwd/8"
+		},
+		{
+			"title": "The X-Files",
+			"year": 1982,
+            "poster": "thexfiles.jpg",
+			"reviews": [
+				{"reviewer": "Dennis", "stars": 3, "text": "Tiramisu sugar plum chocolate bar wafer oat cake cotton candy lollipop croissant donut. Tiramisu donut chocolate muffin marzipan halvah candy halvah. Faworki cotton candy candy faworki candy canes chocolate cake jelly-o fruitcake jelly-o."},
+				{"reviewer": "Kate", "stars": 4, "text": "Pie sweet roll marshmallow bonbon brownie candy fruitcake. Bonbon chupa chups dessert. Sesame snaps candy canes chocolate bar topping."}
+			],
+            "link": "http://bkaprt.com/rwd/9"
+		},
+		{
+			"title":
+			"Two and a Half Men",
+			"year": 1983,
+            "poster": "trueblood.jpg",
+			"reviews": [
+				{"reviewer": "Jennifer", "stars": 3, "text": "Applicake fruitcake tart chocolate bar candy. Applicake croissant liquorice. Jelly-o gummi bears tootsie roll wafer ice cream."},
+				{"reviewer": "Stuart", "stars": 4, "text": "Pastry caramels jujubes applicake jujubes macaroon cake jelly. Lemon drops sweet roll powder sweet gingerbread lemon drops. Bear claw caramels marshmallow carrot cake."}
+			],
+            "link": "http://bkaprt.com/rwd/10"
+		},
+		{
+			"title": "Die Hard",
+			"year": 1984,
+            "poster": "diehard.jpg",
+			"reviews": [
+				{"reviewer": "Dominic", "stars": 3, "text": "Tootsie roll topping halvah cake tart oat cake carrot cake marshmallow marshmallow. Liquorice macaroon powder pudding cotton candy dessert wafer pie. Tootsie roll tart tiramisu bonbon lollipop sesame snaps pudding marshmallow. Icing chocolate oat cake sweet roll drag&eacute;e jujubes pie marshmallow."},
+				{"reviewer": "Brenda", "stars": 4, "text": "Marshmallow chocolate bar marshmallow. Faworki danish icing jujubes lollipop pudding sugar plum. Bonbon muffin lemon drops muffin jelly-o faworki applicake carrot cake danish. Pie bonbon jelly gummi bears sugar plum muffin sweet roll ice cream chocolate bar."}
+			],
+            "link": "http://bkaprt.com/rwd/11"
+		},
+		{
+			"title": "Face Off",
+			"year": 1985,
+            "poster": "faceoff.jpg",
+			"reviews": [
+				{"reviewer": "Linda", "stars": 3, "text": "Faworki tiramisu jelly muffin ice cream cake cake. Bear claw toffee lemon drops brownie sesame snaps lemon drops. Fruitcake sweet sugar plum pie powder."},
+				{"reviewer": "Bert", "stars": 4, "text": "Lollipop liquorice icing bonbon tart tiramisu sweet roll powder. Macaroon toffee gummi bears danish powder drag&eacute;e halvah lollipop. Sugar plum cake macaroon pastry pie macaroon pudding chocolate cake chocolate bar. Liquorice pudding lemon drops pastry lemon drops sweet roll wafer."}
+			],
+            "link": "http://bkaprt.com/rwd/12"
+		},
+		{
+			"title": "Jaws",
+			"year": 1986,
+            "poster": "jaws.jpg",
+			"reviews": [
+				{"reviewer": "Pim", "stars": 2, "text": "Tiramisu bear claw apple pie topping cheesecake lollipop donut toffee chocolate cake. Bear claw icing bear claw lemon drops lemon drops cotton candy faworki fruitcake. Halvah halvah toffee marshmallow donut bonbon caramels chupa chups. Brownie gummies icing cotton candy donut."},
+				{"reviewer": "Alyce", "stars": 1, "text": "Tart danish apple pie. Chocolate cake bear claw sweet danish ice cream chocolate bar. Lollipop chocolate danish apple pie."}
+			],
+            "link": "http://bkaprt.com/rwd/13"
+		}
+	]
+}
--- js/global.js
+++ js/global.js
@@ -5,6 +5,11 @@ window.onload = function() {
 }
 */
 
+var projectConstants = {
+    bannerBase: "images/banners/",
+    posterBase: "images/posters/"
+};
+
 $(document).ready(function() {
 	initialiseTheScripts();
 });
@@ -30,7 +35,7 @@ var initialiseTheScripts = function () {
 
 };
 
-var intialiseGeneral = function () {
+var initialiseGeneral = function () {
     
 };
 
@@ -46,14 +51,33 @@ var initialiseContact = function () {
 
 };
 
-var initialiseProjects = function() {
-	var i, theReviews, j, k;
-	$.getJSON("js/projects.js", function(json) {
-		$.each(json.projects, function (i) {
-			$("body").append("<p>" + json.projects[i].title + "</p>");
-		});
-	})	
-	.success(function() { console.log("Success"); })
-	.error(function() { console.log("Error"); })
-	.complete(function() { console.log("Complete"); });
+var initialiseProjects = function () {
+    var i, j, k;
+    $.getJSON("js/content.js", function (data) {
+        $.each(data.banners, function () {
+            var theText = this.text;
+            $("#mainHeader ").append("<a href='" + this.link + "' target='_blank'>" + "<img src='" + projectConstants.bannerBase + this.banner + "' title='" + theText + "' alt='" + theText + "' /></a>");
+        });
+
+        $.each(data.films, function (i) {
+            var theTitle = this.title;
+            var thePoster = this.poster;
+            var theLink = this.link;
+            var currentFilmTitle = "<h2 class='film-title'>" + theTitle + "</h2>";
+            var currentFilmImage = "<a href='" + theLink + "' class='film-link' target='_blank'>" + "<img src='" + projectConstants.posterBase + thePoster + "' title='" + theTitle + "' alt='" + theTitle + "' /></a>";
+            $("#filmsContent article").append("<div class='film'>" + currentFilmTitle + currentFilmImage + "<h2>Reviews</h2></div>");
+
+            $.each(this.reviews, function () {
+                var currentFilmStars = "<span class='stars'>" + this.stars + " Stars</span>";
+                var currentFilmText = "<p>&#147;" + this.text + "&#148;</p>";
+
+                var currentFilmReviewer = "<span class='reviewer'>Reviewed by " + this.reviewer + "</span>";
+
+                $(".film").eq(i).append(currentFilmStars + currentFilmText + currentFilmReviewer);
+            });
+        });
+    })
+	.success(function () { console.log("Success"); })
+	.error(function () { console.log("Error"); })
+	.complete(function () { console.log("Complete"); });
 };
