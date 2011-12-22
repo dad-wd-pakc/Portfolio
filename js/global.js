e66fd064b6442db517e357c6c7f0988e3ec54740
dad-wd-pakc <pak-loon.cheng@digitalanddirect.com>
2011-12-22 18:05:42 +0000

    Closing in on the finish

--- js/global.js
+++ js/global.js
@@ -21,6 +21,10 @@ var initialiseTheScripts = function () {
             initialiseGeneral();
             initialiseHome();
             break;
+        case "blog":
+            initialiseGeneral();
+            initialiseAbout();
+            break;
         case "about":
             initialiseGeneral();
             initialiseAbout();
@@ -43,6 +47,10 @@ var initialiseHome = function () {
     initialiseProjects();
 };
 
+var initialiseBlog = function () {
+
+};
+
 var initialiseAbout = function () {
 
 };
@@ -52,11 +60,17 @@ var initialiseContact = function () {
 };
 
 var initialiseProjects = function () {
+    //(function () {
+    //alert("It works!");
+    //})();
+
     var i, j, k;
     $.getJSON("js/content.js", function (data) {
         $.each(data.banners, function () {
             var theText = this.text;
-            $("#mainHeader ").append("<a href='" + this.link + "' target='_blank'>" + "<img src='" + projectConstants.bannerBase + this.banner + "' title='" + theText + "' alt='" + theText + "' /></a>");
+            var bannerImage = "<b><img src='" + projectConstants.bannerBase + this.banner + "' title='" + theText + "' alt='" + theText + "' /></b>";
+            var bannerCaption = "<p class='caption'>" + theText + "</p>";
+            $(".slides").append("<li class='hero'>" + bannerImage + bannerCaption + "</li>");
         });
 
         $.each(data.films, function (i) {
@@ -70,14 +84,18 @@ var initialiseProjects = function () {
             $.each(this.reviews, function () {
                 var currentFilmStars = "<span class='stars'>" + this.stars + " Stars</span>";
                 var currentFilmText = "<p>&#147;" + this.text + "&#148;</p>";
-
                 var currentFilmReviewer = "<span class='reviewer'>Reviewed by " + this.reviewer + "</span>";
-
                 $(".film").eq(i).append(currentFilmStars + currentFilmText + currentFilmReviewer);
             });
         });
     })
-	.success(function () { console.log("Success"); })
-	.error(function () { console.log("Error"); })
-	.complete(function () { console.log("Complete"); });
+	.success(function () {
+	    // console.log("Success");
+    })
+	.error(function () {
+	    // console.log("Error");
+    })
+	.complete(function () {
+	    // console.log("Complete");
+    });
 };
